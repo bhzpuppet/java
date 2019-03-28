d7c80c6098c1b26b820515db76c8e544e3267ebf
bhz <1666894192@qq.com>
2019-03-29 01:34:03 +0800

    full solution

--- src/Track.java
+++ src/Track.java
@@ -1,7 +1,9 @@
+import java.awt.SecondaryLoop;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class Track {
 	private Point[] aPoint = new Point[1000];
@@ -46,5 +48,48 @@ public class Track {
 			throw new GPSException ("Index of point out of range");
 		}
 		return aPoint[index];
+	}
+	public Point lowestPoint () throws GPSException {
+		if (numberOfPoint < 1) {
+			throw new GPSException ("no enough points");
+		}
+		ZonedDateTime t1 = ZonedDateTime.parse("2016-02-17T09:52:39Z");
+		Point min = new Point(t1, -1.547720, 53.803941, 10000);
+		for (int x=0; x<numberOfPoint; x++) {
+			if (aPoint[x].getElevation() < min.getElevation() ) {
+				min = aPoint[x];
+			}
+		}
+		return min;
+	}
+	public Point highestPoint () throws GPSException {
+		if (numberOfPoint < 1) {  //exception
+			throw new GPSException ("no enough points");
+		}
+		ZonedDateTime t1 = ZonedDateTime.parse("2016-02-17T09:52:39Z");
+		Point max = new Point(t1, -1.547720, 53.803941, -10000);
+		for (int x=0; x < numberOfPoint; x++) {
+			if (aPoint[x].getElevation() > max.getElevation() ) {
+				max = aPoint[x];
+			}
+		}
+		return max;
+	}
+	public double totalDistance () throws GPSException {
+		if (numberOfPoint < 2) {
+			throw new GPSException ("no enough points");
+		}
+		double totaldistance = 0;
+		for (int x=0; x < numberOfPoint-1; x++) {
+			totaldistance = totaldistance + Point.greatCircleDistance(aPoint[x], aPoint[x+1]);
+		}
+		return totaldistance;
+	}
+	public double averageSpeed () throws GPSException {
+		if (numberOfPoint < 2) {
+			throw new GPSException ("no enough points");
+		}
+		double seconds = ChronoUnit.SECONDS.between(aPoint[0].getTime(), aPoint[numberOfPoint-1].getTime());
+		return this.totalDistance()/seconds;
 	}
 }
